f505e60536e302463c93efe5b22884b6aec74319
Samiya <skmcintosh20@gmail.com>
2022-05-12 14:29:53 -0700

    finished chatbox and functionality

---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Message({message, timestamp, user, userImage}) {
+	return <MessageConatiner>Message</MessageConatiner>;
+}
+
+export default Message;
+
+const MessageConatiner = styled.div``;
